f41ef1eea4afcdd668146609b5102b6e409ce6a1
soni240 <s.fegade21@gmail.com>
2022-05-28 09:52:23 +0530

    [Gunavanti]leap year

---
+++ Workshop Leap Year/Leap Year/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+int year;
+Console.WriteLine("enter the year:");
+year = int.Parse(Console.ReadLine());
+if (year % 4 ==0 && year % 400 == 0)
+{
+    Console.WriteLine(year + "it is leap year");
+}
+else
+{
+    Console.WriteLine(year + "it is not leap year");
+}
